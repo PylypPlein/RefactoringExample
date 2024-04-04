808006f83cf211b5ba40560870ddc602ebda2a41
s24675 <s24675@pjwstk.edu.pl>
2024-04-04 20:08:37 +0200

    Dokonano refaktoryzacji Client.cs.

--- LegacyApp/Client.cs
+++ LegacyApp/Client.cs
@@ -2,10 +2,40 @@
 {
     public class Client
     {
-        public string Name { get; internal set; }
-        public int ClientId { get; internal set; }
-        public string Email { get; internal set; }
-        public string Address { get; internal set; }
-        public string Type { get; set; }
+        private string _name;
+        private int _clientId;
+        private string _email;
+        private string _address;
+        private string _type;
+
+        public string Name 
+        { 
+            get => _name; 
+            internal set => _name = value; 
+        }
+
+        public int ClientId 
+        { 
+            get => _clientId; 
+            internal set => _clientId = value; 
+        }
+
+        public string Email 
+        { 
+            get => _email; 
+            internal set => _email = value; 
+        }
+
+        public string Address 
+        { 
+            get => _address; 
+            internal set => _address = value; 
+        }
+
+        public string Type 
+        { 
+            get => _type; 
+            set => _type = value; 
+        }
     }
 }
--- LegacyApp/obj/Debug/net8.0/LegacyApp.AssemblyInfo.cs
+++ LegacyApp/obj/Debug/net8.0/LegacyApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LegacyApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f08bba9208f0b123e59c7eceb3946271e9948caf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecf5fa58930e959baa05885b79ba6ba1ba1eb213")]
 [assembly: System.Reflection.AssemblyProductAttribute("LegacyApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LegacyApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
